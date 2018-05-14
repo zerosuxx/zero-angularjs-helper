b6679286359738e3fdf2e64e070dfa1a3381635a
Mohos Tamás <tomi@mohos.name>
2018-05-14 18:09:40 +0200

    [Added] ClickHandlerDirective [NewVersion] 1.0.60

---
+++ src/Directive/ClickHandlerDirective.js
@@ -0,0 +1,28 @@
+angular.module('zerosuxx.zeroNgHelper')
+.directive('clickHandler', ['HttpClient', function(HttpClient) {
+    return {
+        restrict: 'C',
+        link: function(scope, element, attrs) {
+            var method = attrs.method || 'GET';
+            var params = attrs.params || {};
+            var key = attrs.key;
+            scope.ajaxHandle = function(url, callback) {
+                HttpClient.request(method, url, params).then(function(response) {
+                    var enable = !scope[key];
+                    if(callback) {
+                        callback(enable, scope, element[0], attrs);
+                    }
+                    if(response.data.status === 'success') { 
+                        scope[key] = enable;
+                    }
+                });
+            };
+            
+            scope.handle = function(callback) {
+                var enable = !scope[key];
+                callback(enable, scope, element[0], attrs);
+                return true;
+            };
+        }
+    };
+}]);
